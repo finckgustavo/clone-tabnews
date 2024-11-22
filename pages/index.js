65c4e00e9b15212f96462d32c6664fc654c7e8c4
Gustavo Finck <finckgustavo@gmail.com>
2024-11-22 01:09:44 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Parece que agora realmente é um site!</h1>
+  return <h1>Parece que agora realmente é um site!</h1>;
+}
+
+function test() {
+  console.log("test");
 }
 
 export default Home;
